c5084fcbe8b8da72b606bd273a937f290aa8be18
rodrigoalvarellos <rodri.alvarellos@gmail.com>
2019-09-16 15:45:49 -0300

    Rest server - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,24 @@
 // PUERTO
 // ====================
 process.env.PORT = process.env.PORT || 3000;
+
+// ====================
+// ENTORNO
+// ====================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ====================
+// Base de datos
+// ====================
+let local = 'mongodb://localhost:27017/cafe';
+let remota = 'mongodb+srv://ralvarellos69:H4293tS2JN3PJ2Y7@cluster0-alygm.mongodb.net/cafe';
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = local;
+} else {
+    urlDB = remota;
+}
+
+// seteo la variable en el process enviroment
+process.env.urlDB = urlDB;
